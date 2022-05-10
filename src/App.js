f74d8e3782a86fecb7fdd62364ce7596d2109310
Alexey Piltenko <ukr.top@ukr.net>
2022-05-10 16:06:27 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-//Используем App.css
 import './App.css';
 import randomColor from 'randomcolor';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
   const [color, setColor] = useState(randomColor.randomColor());
-
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
+  const generateColor = randomColor({ hue: hue, luminosity: luminosity });
   return (
     <div className="App">
       <h1>Random Color</h1>
       <h2>React</h2>
       <div
         style={{
-          backgroundColor: color,
+          backgroundColor: generateColor,
           width: '100%',
-          height: '400px',
+          height: '90px',
           display: 'flex',
           fontWeight: 'bold',
           alignItems: 'center',
           justifyContent: 'center',
         }}
       >
-        <button onClick={() => setColor(randomColor.randomColor())}>
-          Generated Color: {color}
-        </button>
+        Generated Color: {generateColor}
       </div>
-      <button onClick={() => setColor(randomColor.randomColor())}>
-        Generate
-      </button>
-      <h2>Push!</h2>
+      <br />
+      <br />
+      <button onClick={() => setColor(generateColor)}>Generate</button>
+      <br />
+      <br />
+      <div>
+        <h3>Hue:</h3>
+        <input
+          value={hue}
+          onChange={(event) => setHue(event.currentTarget.value)}
+        />
+        <br />
+        <br />
+        <h3>Luminosity:</h3>
+        <input
+          value={luminosity}
+          onChange={(event) => setLuminosity(event.currentTarget.value)}
+        />
+      </div>
     </div>
   );
 }
